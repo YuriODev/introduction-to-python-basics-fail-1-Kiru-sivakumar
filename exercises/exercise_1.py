23acf9c0fbd28a7c8b34b18a1662e3d46e66557d
Kiru-sivakumar <157816684+Kiru-sivakumar@users.noreply.github.com>
2024-03-25 11:58:31 +0000

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,4 +1,15 @@
 # Exercise 1
 # Your solution comes here
-num = int(input("Please enter a five digit number"))
-print(len(str(num))
+
+num = int(input('Please enter a five digit number'))
+first = (num // 10000) % 10
+second = (num // 1000) % 10
+third =  (num // 100) % 10
+fourth = (num // 10) % 10
+fifth =  (num) % 10
+print(first , second, third, fourth, fifth)
+
+new_num = str(first + third + fifth) + str(second + fourth) 
+
+print(new_num)
+
