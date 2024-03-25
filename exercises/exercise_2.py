2e19474f50e92caae8fe9b8deacd2649719819fb
Kiru-sivakumar <157816684+Kiru-sivakumar@users.noreply.github.com>
2024-03-25 12:05:58 +0000

    Update exercise_2.py

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,9 @@
 # Exercise 2
 # Your solution comes here
+num = int(input('Please enter your number:'))
+if num % 2 != 0:
+  print(num + 1)
+if num % 2 == 0:
+  print(num + 2 )
+if num == 0:
+  print(num +2)
